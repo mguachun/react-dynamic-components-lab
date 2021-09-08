3afae38611e294cfc5a5d4e415d07d14ae860bee
Melissa Guachun <1212malka@gmail.com>
2021-09-08 10:59:09 -0400

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -21,8 +21,8 @@ export default class BlogPost extends Component {
 
         (remove the comment ticks and their surrounding brackets)
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
        
       </div>
     );
